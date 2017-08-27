cb78022e27d8192683274f395c51bdf4c2c880b5
Kimberley Zell <kimberley.zell@gmail.com>
2017-08-26 19:51:52 -0700

    Corrected compute method to be a self method. Now tests pass. Project complete.

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -1,38 +1,36 @@
 class Hamming
   attr_reader :strand
 
-  def initialize(strand1, strand2)
-    @strand1 = strand1
-    @strand2 = strand2
+  def initialize
+    @strand = strand
+
   end
 
-  def compute  #compare the two strands
+  def self.compute(strand1, strand2)  #compare the two strands
     length = nil
-    if @strand1.length == @strand2.length
-      equal_length = @strand1.length
+    if strand1.length == strand2.length
+      length = strand1.length
     else
       raise ArgumentError.new("Strands must be equal in length in order to compare")
     end
 
-    strand1_arr = @strand1.split("")
-    strand2_arr = @strand2.split("")
+    strand1_arr = strand1.upcase.split("")
+    strand2_arr = strand2.upcase.split("")
 
-    @distance = 0
+    distance = 0
     i = 0
 
     strand1_arr.each do |value|
       if value != strand2_arr[i]
-        @distance += 1
+        distance += 1
       end
         i += 1
     end
 
-    puts @distance
+    return distance
 
   end
 
 end
-
-hamming = Hamming.new("AAAACA", "ABABCB")
-puts hamming
-puts hamming.compute
+#
+#puts Hamming.compute("aba", "bca")
